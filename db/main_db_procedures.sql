732f7e174ef74a4be9c9d2fd1efd93cad13a74c9
BlauRick <157022879+BlauRick@users.noreply.github.com>
2026-01-16 12:12:04 +0100

    Update main_db_procedures.sql

--- db/main_db_procedures.sql
+++ db/main_db_procedures.sql
@@ -1,1 +1,11 @@
+DELIMITER //
+CREATE OR REPLACE PROCEDURE ordayna_main_db.detach_intezmeny(
+	intezmeny_id_to_detach INT UNSIGNED
+) BEGIN
+	DELETE FROM foadatbazis.intezmeny_ids
+	WHERE intezmeny_ids.intezmeny_id = intezmeny_id_to_detach
+	;
+END;
+//
+DELIMITER ;
 
